385959285916095be298e3306e22abe5c726fa0b
Gustakx36 <gustavobruno12333@gmail.com>
2023-12-28 22:21:23 -0300

    Alterações node v39

--- functions/api.js
+++ functions/api.js
@@ -11,7 +11,7 @@ let records = [];
 
 //Get all students
 router.get('/', (req, res) => {
-  res.sendFile(__dirname + "dist/index.html");
+  res.sendFile(__dirname + "/dist/index.html");
 });
 
 //Create new record
